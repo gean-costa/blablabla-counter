07e4200112418e9f86e2fed7b8ae71040fb23721
Gean Costa <geandreson.costa@gmail.com>
2020-12-05 16:48:55 -0300

    Implementação da wordcloud.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 from classes.user import User
 from classes.word import Word
 
@@ -16,6 +18,12 @@ def main():
 
     with open('data/word_frequencies.json', 'w') as file:
         json.dump(words.word_frequencies, file, indent=3, ensure_ascii=False)
+
+    wordcloud = WordCloud(
+        max_words=4000).generate_from_frequencies(words.word_frequencies)
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
 
 
 if __name__ == "__main__":
